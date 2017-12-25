93b9f23156b8fe40b5c16a84a102cb0a979f10b1
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-25 16:06:46 +0530

    slider not showing properly

--- src/components/home.js
+++ src/components/home.js
@@ -1,36 +1,41 @@
-import React,{Component} from 'react';
+import React,{ Component } from 'react';
 
-//components
+// COMPONENTS
 import Featured from './featured';
+//import Subscriptions from './subscriptions';
+//import Blocks from './blocks';
+//import Poll from './poll';
 
-const URL_HOME='http://localhost:3004/home'
+const URL_HOME = 'http://localhost:3004/home';
 
-class Home extends Component{
-
+class Home extends Component {
     constructor(props){
-        super(props);
-        this.state={
-            home:''
+        super(props)
+
+        this.state = {
+            home:'',
+            poll:[]
         }
     }
 
-    componentDidmount(){
-        fetch(URL_HOME,{method:'GET'})
-        .then(response=>response.json())
-        .then(json=>{
-            this.setState({
-                home:json
-            })
+    componentDidMount() {
+        fetch(URL_HOME, { method: 'GET'})
+        .then(response => response.json())
+        .then(json => {
+            this.setState({home:json})
         })
     }
+
 
     render(){
         return(
             <div>
                 <Featured slides={this.state.home.slider}/>
             </div>
+   
         )
     }
 }
+
 
 export default Home;
