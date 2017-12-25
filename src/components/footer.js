483b299a98fa1d8320e47a52521483416f4a846d
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-25 14:41:42 +0530

    setting up routes

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import header from './header';
+
+const Footer=()=>{
+    return(
+        <header>
+            <div>Footer</div>
+        </header>
+    )
+}
+
+export default Footer;
