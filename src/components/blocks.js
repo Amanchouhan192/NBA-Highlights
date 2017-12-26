13248ab80f76d16702f242f995f059dac15f52e3
rishabh-malik <rishabhmalik249@gmail.com>
2017-12-26 13:47:53 +0530

    generating blocks

--- src/components/blocks.js
+++ src/components/blocks.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Reveal from 'react-reveal';
+import 'animate.css/animate.css';
+import {Link} from 'react-router-dom';
+
+const generateBlocks=({blocks})=>{
+    if(blocks){
+        return blocks.map((item)=>{
+            return(
+                <Reveal key={item.id} effect="animated fadeInUp">
+                    text
+                </Reveal>
+            )
+        })
+    }
+}
 
 const Blocks=(props)=>{
     return(
-        <div>
-            {}
+        <div className="home_blocks">
+            {generateBlocks(props)}
         </div>
     )
 }
